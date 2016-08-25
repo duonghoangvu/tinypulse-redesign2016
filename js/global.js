48844a4d514033fd5d93310f8f816691928390a8
Duong Hoang Vu <vuduong@tinypulse.com>
2016-08-25 10:26:29 +0700

    updated subnav

--- js/global.js
+++ js/global.js
@@ -1,21 +1,32 @@
 jQuery(document).ready(function($) {
 	var subnav = $('.subnav'), 
 		body = $('body');
+
   $('.main-mobile-nav-container nav').height($(window).height());
   
   $('.hamburger').click(function() {
-      $('body').addClass('mobile-nav-opened');
+    $('body').addClass('mobile-nav-opened');
   });
   
   $(".cross").click(function() {
-      $('body').removeClass('mobile-nav-opened');
+    $('body').removeClass('mobile-nav-opened');
   });
   
   $(window).on('resize', function(){
-  	if ($(window).width() < 960){
+    if ($(window).width() < 960){
 			$('#site-footer').find('ul li:first-child').off('click').on('click', function(e){
 				$(this).closest('ul').toggleClass('expanded');
-			})
+			});
+      $('#sub-nav-trigger').off('click').on('click', function(e){
+      	e.preventDefault();
+	  		$(this).closest('ul').toggleClass('expanded');
+	  	});
+      $('.subnav').find('a:not(#sub-nav-trigger)').off('click').on('click', function(e){
+        e.preventDefault();
+        $('.subnav li').removeClass('active');
+        $(this).closest('li').addClass('active');
+        $('#sub-nav-trigger').trigger('click');
+      });
 		}
 	}).on('scroll', function(){
 		var scrollTop = $(window).scrollTop();
@@ -24,7 +35,7 @@ jQuery(document).ready(function($) {
 		} else {
 			body.removeClass('subnav-sticked');
 		}
-	});
+	}).trigger('resize');
 
 	var scroller = new Scroller();
 	scroller.dom.blur = $('header .overlay');
@@ -88,6 +99,7 @@ Scroller.prototype = {
     if ($(window).width() < 960){
       return;
     }
+
     var slowScroll = (currentScrollY - ($(window).height() + 300)) / 4
       , blurScroll = currentScrollY * 1.5;
 
@@ -95,14 +107,16 @@ Scroller.prototype = {
       'opacity' : blurScroll / this.wHeight
     });
 
-    $('.masked-image-2').css({
-      'transform': 'translateY(-' + slowScroll + 'px)'
-    });
+    if (window.location.pathname === '/' || window.location.pathname === '/redesign-2016-homepagezaqwsxcderfv'){
+      $('.masked-image-2').css({
+        'transform': 'translateY(-' + slowScroll + 'px)'
+      });
 
-    if (currentScrollY + 76 < $('.text-2').offset().top){
-      $('.masked-image-1').css({
-        'transform': 'translateY(-' + slowScroll/2 + 'px)'
-      });      
+      if (currentScrollY + 76 < $('.text-2').offset().top){
+        $('.masked-image-1').css({
+          'transform': 'translateY(-' + slowScroll/2 + 'px)'
+        });      
+      }      
     }
   }
 };
