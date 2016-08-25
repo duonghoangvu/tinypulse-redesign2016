94147d7a52b9ba71bcadea72f64f1e30f499149c
lamphuongha <phuong@tinypulse.com>
2016-08-26 00:18:38 +0700

    contact us page

---
+++ js/contact.js
@@ -0,0 +1,14 @@
+jQuery(document).ready(function($) {
+  function callErrorMessage() {
+    if ($('form.hs-form').length > 0) {
+      $('form.hs-form').on('submit', function(){
+        if($('input[name="hs_context"]').val() != '') {
+          $('.alert-error').attr('style','opacity: 1');
+        }
+      });
+    } else {
+      setTimeout(callErrorMessage, 500);
+    }
+  }
+  callErrorMessage();
+});
