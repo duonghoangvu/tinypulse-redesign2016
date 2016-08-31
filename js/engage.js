40945c9d7914f028ba6f56acb02a87279d5c4e6b
lamphuongha <phuong@tinypulse.com>
2016-08-30 20:23:24 -0700

    fix engage hover effect

--- js/engage.js
+++ js/engage.js
@@ -55,4 +55,28 @@ jQuery(document).ready(function($) {
   $('.next-speaker').click(function(){
     showNextBlock('.speaker', $(this).parent());
   });
+  $(".overview-block").click(function(e){
+    $(".overview-block").removeClass('selected');
+    $(this).addClass('selected');
+  });
+  $(".overview-block").hover(function(){
+    $('.overview-img').attr('style', 'display: none');
+    $(this).find('.overview-img').attr('style', 'display: block;transition: all ease .5s;');
+  });
+  $(".overview-content").mouseleave(function(e){
+    $('.overview-img').attr('style', 'display: none');
+    $(this).find('.overview-block.selected .overview-img').attr('style', 'display:block');
+  });
+  $(".hiw-icon").click(function(e){
+    $(".hiw-content").removeClass('selected');
+    $(this).parent().addClass('selected');
+  });
+  $(".hiw-content").hover(function(){
+    $('.block-content').attr('style', 'display: none');
+    $(this).find('.block-content').attr('style', 'display:block;transition: all .3s;');
+  });
+  $(".hiw-icons").mouseleave(function(e){
+    $('.block-content').attr('style', 'display: none');
+    $(this).find('.hiw-content.selected .block-content').attr('style', 'display:block;transition: all .3s;');
+  });
 });
