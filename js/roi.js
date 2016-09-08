151059ba559412c5a79964ee8709de902b1432d6
Ruby <ngoc@tinypulse.com>
2016-09-08 15:18:06 +0700

    Apply formula for ROI

--- js/roi.js
+++ js/roi.js
@@ -11,33 +11,63 @@ jQuery(document).ready(function(){
     $(".details").slideUp("slow");
   });
 
+  function is_numeric(event) {
+    if (event.keyCode == 190){
+          return;
+    }else {
+      // If it's not a number stop the keypress
+      if (event.shiftKey || (event.keyCode < 48 || event.keyCode > 57) && (event.keyCode < 96 || event.keyCode > 105 )) {
+          event.preventDefault(); 
+      }   
+    }
+  }
+
+  $('#number-of-employees').keydown(function(event) {
+    is_numeric(event);
+  });
+
+  $('#average-salary').keydown(function(event) {
+    is_numeric(event);
+  });
+  
+  $('#yearly-turnover').keydown(function(event) {
+    is_numeric(event);
+  });
+  
   function calculateROI() {
 
     var num_employees = $('#number-of-employees').val();
     var average_salary = $('#average-salary').val();
     var average_turnover = $('#yearly-turnover').val();
-    var reduction = $('.rangeSlider__handle').data('content');
+    var reduction = $('.rangeSlider__handle').data('content')/100;
 
     var scale = 2.13;
     if(average_salary < 75001) {
       scale = 0.2;
     } if(average_salary < 30001) {
       scale = 0.16;
-    } 
-
-    console.log("num_employees: " + num_employees + " -- average_salary " + average_salary + " -- average_turnover " + average_turnover + " -- reduction" + reduction + " -- scale" + scale);
+    }
 
     var cost_to_rehire = (num_employees * average_turnover * average_salary * scale) / 100;
-    var cost_of_tinypulse = num_employees*10;
-    var cost_to_rehire_with_tinypulse = num_employees*(1 - reduction)*average_turnover*average_salary*scale/100100;
-    var number_employees_saved = 
+    var cost_of_tinypulse = num_employees*12*10;
+    var reduced_cost_to_rehire_with_tinypulse = num_employees*(1 - reduction)*average_turnover*average_salary*scale/(100*100);
+
+    console.log( 'average_turnover ' + average_turnover)
+    var number_employees_saved = (num_employees*average_turnover/100) - num_employees*(1-reduction)*average_turnover/100;
+    var saving = cost_to_rehire - cost_of_tinypulse - reduced_cost_to_rehire_with_tinypulse;
+
     $('#cost-to-rehire').text(cost_to_rehire);
     $('#cost-of-tinypulse').text(cost_of_tinypulse);
-    $('#reduced-cost-to-rehire-with-tinypulse').text(cost_to_rehire_with_tinypulse);
-    $('#annual-save-using-tinypulse').text('$123456789');
+    $('#reduced-cost-to-rehire-with-tinypulse').text(reduced_cost_to_rehire_with_tinypulse);
+    $('#number-employees-saved').text(Math.ceil(number_employees_saved));
+    $('#annual-save-using-tinypulse').text(saving);
 
-    // Number Employees Saved: (numEmployeesturnover/100) - numEmployees(1-reduced)*turnover/100
-    // Savings: (Cost to Rehire) - (Cost of TINYpulse) - (Reduced Cost to Rehire)
+    // Format currency
+    $('#annual-save-using-tinypulse').formatCurrency();
+    $('#cost-to-rehire').formatCurrency();
+    $('#cost-of-tinypulse').formatCurrency();
+    $('#reduced-cost-to-rehire-with-tinypulse').formatCurrency();
+    $('#annual-save-using-tinypulse').formatCurrency();
   }
 
   $('#number-of-employees').change(function(){
@@ -71,19 +101,14 @@ jQuery(document).ready(function(){
       stick: 25,        // [Number stickTo, Number stickRadius] : use it if handle should stick to stickTo-th value in stickRadius
       borderRadius: 10,    // Number, if you use buffer + border-radius in css for looks good,
       onInit: function () {
-        console.info('onInit');
         $('.rangeSlider__handle').attr('data-content', '74');
       },
       onSlideStart: function (position, value) {
-          console.info('onSlideStart', 'position: ' + position, 'value: ' + value);
       },
       onSlide: function (position, value) {
-        console.log('onSlide', 'position: ' + position, 'value: ' + value);
         $('.rangeSlider__handle').attr('data-content', position);
-
       },
       onSlideEnd: function (position, value) {
-        console.warn('onSlideEnd', 'position: ' + position, 'value: ' + value);
         calculateROI();
       }
   });
